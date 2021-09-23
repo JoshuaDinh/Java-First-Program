33a643bd9d3e9c4705852df6994806e77d75aeb3
Joshua Dinh <joshuahungdinh@gmail.com>
2021-09-23 09:50:32 -0700

    updated with primitives

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, . how are you doing!");
-    }
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        System.out.println(age);
+        System.out.println(viewsCount);
+        System.out.println(price);
+        System.out.println(letter);
+        System.out.println(isEligible);
+    } 
 }
